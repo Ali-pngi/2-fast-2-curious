154e52f194e4b9f21bbce1894d5079cccd57608a
Ali <belitherali@gmail.com>
2024-07-12 18:11:44 +0100

    added basic file structure and server connection

---
+++ server.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+const authController = require('./controllers/auth.js')
+const listingsController = require('./controllers/listings.js')
+
+const port = process.env.PORT ? process.env.PORT : '3000'
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+app.use(morgan('dev'))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    })
+  })
+)
